ea73828673c114c1d8f7e6d62197048e31f8fb96
emiliaMetelitsa <klthsrswm@gmail.com>
2026-01-07 22:23:26 +0300

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.api.routes import router
+from app.db.session import engine
+from app.db.models import Base
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Students API", version="1.0.0")
+app.include_router(router)
